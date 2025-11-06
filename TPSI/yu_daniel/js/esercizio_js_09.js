ce384a50116ab722f3ca63527b99d441760646de
Yu-Daniel <liuweidaniel.yu@istitutoagnelli.it>
2025-11-06 16:41:03 +0100

    ciao

---
+++ TPSI/yu_daniel/js/esercizio_js_09.js
@@ -0,0 +1,6 @@
+let pass;
+let usr = (prompt("nome") ) ?? alert("cancelled");
+(usr == null || usr == "admin" || alert("idku")) ;
+(usr == null || usr != "admin" || (pass = prompt("password"))) ?? alert("cancelled");
+(pass == null || pass == "agnell1no" || alert("password errata")); 
+(pass != "agnell1no" || pass == null || alert("welcome")); 
